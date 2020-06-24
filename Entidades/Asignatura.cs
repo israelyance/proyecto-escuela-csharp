eadc695d1b7fa2820f86fd8a622f6450a32dcf0a
Israel Yance <israelyance@gmail.com>
2020-06-24 18:52:29 -0500

    Proyecto base final

---
+++ Entidades/Asignatura.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace CoreEscuela.Entidades
+{
+    public class Asignatura
+    {
+        public string UniqueId { get; private set; }
+        public string Nombre { get; set; }
+        public List<Evaluaciones> Evaluaciones { get; set; }
+        public Asignatura() => UniqueId = Guid.NewGuid().ToString();
+    }
+}
